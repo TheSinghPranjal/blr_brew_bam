e3075d6afbe127c9017001e889d26985270b1991
pranjalsingh <pranjalsingh10061997@gmail.com>
2026-05-09 00:28:13 +0530

    team repo added

--- lib/core/auth_service.dart
+++ lib/core/auth_service.dart
@@ -20,6 +20,7 @@ class AuthTokenClaims {
   final List<String> groups;
 
   const AuthTokenClaims({required this.scalars, required this.groups});
+  
 
   factory AuthTokenClaims.empty() =>
       const AuthTokenClaims(scalars: {}, groups: []);
