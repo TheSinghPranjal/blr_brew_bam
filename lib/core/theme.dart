e5935184bcfdf94a6e07bbd3abd9d06f18e0fd7a
pranjalsingh <pranjalsingh10061997@gmail.com>
2026-06-02 23:34:39 +0530

    theme

--- lib/core/theme.dart
+++ lib/core/theme.dart
@@ -79,6 +79,7 @@ class AppTheme {
           borderRadius: BorderRadius.circular(12),
           borderSide: const BorderSide(color: AppColors.primary, width: 2),
         ),
+
         errorBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(12),
           borderSide: const BorderSide(color: AppColors.error),
