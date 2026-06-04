38036c45b1af39fe4e262c90177634d5cf7feb67
pranjalsingh <pranjalsingh10061997@gmail.com>
2026-06-04 12:38:24 +0530

    theme

--- lib/core/theme.dart
+++ lib/core/theme.dart
@@ -102,6 +102,7 @@ class AppTheme {
           textStyle: GoogleFonts.outfit(fontWeight: FontWeight.bold, fontSize: 16),
         ),
       ),
+
       outlinedButtonTheme: OutlinedButtonThemeData(
         style: OutlinedButton.styleFrom(
           foregroundColor: AppColors.primary,
